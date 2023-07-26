08b305382764b9a4d6133e54011a31069b3eb9ae
Anthony Davis Yeren Martinez <ayerenmartinez@hotmail.com>
2023-07-26 16:18:48 -0500

    Proyecto biblioteca egg

--- biblioteca/src/main/java/com/egg/biblioteca/servicios/LibroServicio.java
+++ biblioteca/src/main/java/com/egg/biblioteca/servicios/LibroServicio.java
@@ -3,6 +3,7 @@ package com.egg.biblioteca.servicios;
 import com.egg.biblioteca.entidades.Autor;
 import com.egg.biblioteca.entidades.Editorial;
 import com.egg.biblioteca.entidades.Libro;
+import com.egg.biblioteca.excepciones.MiException;
 import com.egg.biblioteca.repositorios.AutorRepositorio;
 import com.egg.biblioteca.repositorios.EditorialRepositorio;
 import com.egg.biblioteca.repositorios.LibroRepositorio;
@@ -27,7 +28,8 @@ public class LibroServicio {
     private EditorialRepositorio editorialRepositorio;
 
     @Transactional
-    public void crearLibro(Long isbn, String titulo, Integer ejemplares, String idAutor, String idEditorial){
+    public void crearLibro(Long isbn, String titulo, Integer ejemplares, String idAutor, String idEditorial) throws MiException{
+        validar(isbn,titulo,ejemplares,idAutor,idEditorial);
         Autor autor = autorRepositorio.findById(idAutor).get();
         Editorial editorial = editorialRepositorio.findById(idEditorial).get();
         Libro libro = new Libro();
@@ -46,7 +48,8 @@ public class LibroServicio {
         return  libros;
     }
 
-    public void modificarLibro(Long isbn, String titulo, String idAutor, String idEditorial, Integer ejemplares){
+    public void modificarLibro(Long isbn, String titulo, String idAutor, String idEditorial, Integer ejemplares) throws MiException{
+        validar(isbn,titulo,ejemplares,idAutor,idEditorial);
         Optional<Libro> respuesta = libroRepositorio.findById(isbn);
         Optional<Autor> respuestaAutor = autorRepositorio.findById(idAutor);
         Optional<Editorial> respuestaEditorial = editorialRepositorio.findById(idEditorial);
@@ -69,5 +72,25 @@ public class LibroServicio {
             libro.setEditorial(editorial);
             libroRepositorio.save(libro);
         }
+    }
+
+    private void validar(Long isbn, String titulo, Integer ejemplares, String idAutor, String idEditorial) throws MiException{
+
+        if(isbn == null){
+            throw new MiException("El isbn no puede ser nulo");
+        }
+
+        if(titulo.isEmpty() || titulo == null){
+            throw new MiException("El titulo no puede ser nulo o estar vacío");
+        }
+
+        if(ejemplares == null){
+            throw new MiException("ejemplares no puede ser nulo");
+        }
+
+        if(idEditorial.isEmpty() || idEditorial == null){
+            throw new MiException("El idEditorial no puede ser nulo o estar vacío");
+        }
+
     }
 }
